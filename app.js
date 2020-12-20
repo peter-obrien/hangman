4c9de9a91a39432e4ec82b17ee38948ccadf1bd6
Peter O'Brien <pjo2412@gmail.com>
2020-12-20 17:59:30 -0500

    Initial version

---
+++ app.js
@@ -0,0 +1,80 @@
+const wordToGuess = document.querySelector('#wordToGuess')
+const wordForm = document.querySelector('#wordForm')
+const reset = document.querySelector('#resetBtn')
+const guessMode = document.querySelector('#guessMode')
+const wordContainer = document.querySelector('#wordContainer')
+const maxGuesses = 11
+let incorrectGuesses = 0
+wordForm.addEventListener('submit', evt => {
+    evt.preventDefault()
+    let enteredWord = wordToGuess.value
+    for (let i = 0; i < enteredWord.length; i++) {
+        let letterHtml = `<div class="col-"><input type="text" class="unknownLetter placeholder${i} form-control" disabled></div>`
+        wordContainer.innerHTML = wordContainer.innerHTML + letterHtml
+    }
+    guessMode.classList.remove('d-none')
+    wordForm.classList.toggle('d-none')
+    updateGraphic()
+})
+reset.addEventListener('click', resetGame)
+let letterButtons = document.querySelectorAll('.letter')
+for (const letterButton of letterButtons) {
+    letterButton.addEventListener('click', handleGuess)
+}
+
+function resetGame() {
+    guessMode.classList.add('d-none')
+    wordToGuess.value = ''
+    wordForm.classList.toggle('d-none')
+    wordContainer.innerHTML = ''
+    incorrectGuesses = 0
+    for (const letterButton of letterButtons) {
+        letterButton.disabled = false
+    }
+}
+
+function handleGuess(evt) {
+    evt.preventDefault()
+    evt.target.disabled = true
+    if (!validateLetter(evt.target.innerText)) {
+        incorrectGuesses++
+        updateGraphic()
+    }
+    if (isGameWon()) {
+        alert("Winner!")
+        resetGame()
+    }
+    if (isGameOver()) {
+        alert("Game Over!")
+        resetGame()
+    }
+}
+
+function validateLetter(letter) {
+    let result = false
+    const enteredWord = wordToGuess.value.toUpperCase()
+    for (let i = 0; i < enteredWord.length; i++) {
+        if (enteredWord.charAt(i) === letter) {
+            result = true
+            document.querySelector(`.placeholder${i}`).value = enteredWord.charAt(i)
+        }
+    }
+    return result
+}
+
+function isGameWon() {
+    for (const unknownLetter of document.querySelectorAll('.unknownLetter')) {
+        if (!unknownLetter.value) {
+            return false
+        }
+    }
+    return true
+}
+
+function isGameOver() {
+    return incorrectGuesses === maxGuesses
+}
+
+function updateGraphic() {
+    document.querySelector('#graphic').setAttribute('src', `./images/${incorrectGuesses}.png`)
+}
